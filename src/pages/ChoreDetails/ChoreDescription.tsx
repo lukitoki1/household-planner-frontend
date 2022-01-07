79ecb4bff0273e26e2d3ba6cbee920015fec1481
Łukasz Kamiński <lukasz.kaminski@ochk.pl>
2022-01-07 17:02:32 +0100

    Minor tweaks

--- src/pages/ChoreDetails/ChoreDescription.tsx
+++ src/pages/ChoreDetails/ChoreDescription.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 import { ChoreDTO } from '../../api/dto';
-import { Center, HStack, Select, Spinner, Text } from '@chakra-ui/react';
+import { Center, Flex, HStack, Select, Spinner, Text } from '@chakra-ui/react';
 import { languageTranslations } from '../../values';
 import { useQuery } from 'react-query';
 import { Queries } from '../../api/queries';
@@ -39,25 +39,27 @@ export const ChoreDescription: FC<ChoreDescriptionProps> = ({ chore }) => {
 
   return (
     <>
-      <Text fontSize="2xl" marginBottom="8">
-        Opis
-      </Text>
-      <HStack spacing="2" marginBottom="8">
-        <Text>Język: </Text>
-        <Select
-          width="auto"
-          value={language}
-          onChange={(e) => {
-            setLanguage(e.target.value);
-          }}
-        >
-          {languageEntries.map((entry) => (
-            <option key={entry[0]} value={entry[0]}>
-              {entry[1]}
-            </option>
-          ))}
-        </Select>
-      </HStack>
+      <Flex justify="space-between" align="center">
+        <Text fontSize="2xl" marginBottom="8">
+          Opis obowiązku
+        </Text>
+        <HStack spacing="2" marginBottom="8">
+          <Text>Język: </Text>
+          <Select
+            width="auto"
+            value={language}
+            onChange={(e) => {
+              setLanguage(e.target.value);
+            }}
+          >
+            {languageEntries.map((entry) => (
+              <option key={entry[0]} value={entry[0]}>
+                {entry[1]}
+              </option>
+            ))}
+          </Select>
+        </HStack>
+      </Flex>
       {description}
     </>
   );
